0c9d7c3fc313fdb9f832a58ee529c0fda9a766b1
Ovaphlow <jsfreecss@163.com>
2013-06-24 17:28:15 +0800

    Signed-off-by: Ovaphlow <jsfreecss@163.com>

--- phase_log.py
+++ phase_log.py
@@ -1,27 +1,27 @@
 #coding=utf-8
 
 import mysql.connector
-from get_time import get_time
+import funx
 
 cnx1_cfg = {
   'user': 'root',
-  'password': '',
+  'password': 'dsdfjk',
   'host': '127.0.0.1',
   'database': 'ems-hrb',
 }
 
 cnx2_cfg = {
   'user': 'root',
-  'password': '',
+  'password': 'dsdfjk',
   'host': '127.0.0.1',
   'database': 'eiac-sys',
 }
 
 def init_data():
-  sql_text2 = 'DELETE FROM phase_b_log'
+  sql_text2 = 'TRUNCATE TABLE phase_b_log'
   cursor2.execute(sql_text2)
   cnx2.commit()
-  print get_time(), '目标数据表已清空'
+  print funx.get_time(), '目标数据表已清空'
 
 def log_yushouli(row, id):
   sql_text2 = ('INSERT INTO phase_b_log '
@@ -132,22 +132,22 @@ def log_shangbao(row, id):
   cnx2.commit()
 
 if __name__ == '__main__':
-  print get_time(), '初始化...'
+  print funx.get_time(), '初始化...'
   cnx1 = mysql.connector.Connect(**cnx1_cfg)
   cursor1 = cnx1.cursor()
-  print get_time(), '源数据库已连接'
+  print funx.get_time(), '源数据库已连接'
 
   cnx2 = mysql.connector.Connect(**cnx2_cfg)
   cursor2 = cnx2.cursor()
-  get_time()
-  print get_time(), '目标数据库已连接'
+  funx.get_time()
+  print funx.get_time(), '目标数据库已连接'
 
   init_data()
 
   sql_text1 = 'SELECT * FROM tbs001_developprojectbasicinfo'
   cursor1.execute(sql_text1)
   data1 = cursor1.fetchall()
-  print get_time(), '源数据数:', cursor1.rowcount, '开始添加数据到目标数据表'
+  print funx.get_time(), '源数据数:', cursor1.rowcount, '开始添加数据到目标数据表'
 
   for rowData in data1:
     sql_text2 = 'SELECT * FROM phase_a1 WHERE XiangMuMingCheng="' + rowData[0] + '"'
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     data2 = cursor2.fetchall()
     pid = data2[0][0]
     if cursor2.rowcount != 1:
-      print get_time(), p_name, '数据出错！'
+      print funx.get_time(), '编号', pid, '数据出错！'
     log_yushouli(rowData, pid)
     log_shouli(rowData, pid)
     log_bumen(rowData, pid)
@@ -165,13 +165,13 @@ if __name__ == '__main__':
     log_pinggu(rowData, pid)
     log_kaoping(rowData, pid)
     log_shangbao(rowData, pid)
-    print get_time(), rowData[0], '处理完毕'
+    print funx.get_time(), '编号', pid,'添加完毕'
 
-  print get_time(), '所有数据添加完毕', '准备删除无效数据'
+  print funx.get_time(), '所有数据添加完毕', '准备删除无效数据'
   sqlText2 = 'DELETE FROM phase_b_log WHERE account is NULL'
   cursor2.execute(sqlText2)
   cnx2.commit()
-  print get_time(), '无效数据清理完毕'
+  print funx.get_time(), '无效数据清理完毕'
   cursor1.close()
   cnx1.close()
   cursor2.close()
